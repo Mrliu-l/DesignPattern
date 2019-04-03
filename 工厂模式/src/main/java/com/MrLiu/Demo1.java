513566225c5306f22666d0ee289a3eb26fdefb10
Mr.Liu <liu_lei_programmer@163.com>
2019-04-03 10:45:37 +0800

    工厂模式

---
+++ 工厂模式/src/main/java/com/MrLiu/Demo1.java
@@ -0,0 +1,12 @@
+package com.MrLiu;
+
+/**
+ * @author liu_l
+ * @email: liu_lei_programmer@163.com
+ * @time 2019/4/2 17:13
+ * @Description: 描述:
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+    }
+}
