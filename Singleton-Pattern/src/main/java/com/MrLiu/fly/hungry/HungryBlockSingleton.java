6136212fa8e64fb9d17504d93435a215c0caec0a
Mr.Liu <liu_lei_programmer@163.com>
2019-04-16 16:53:46 +0800

    设计模式—单例模式

--- Singleton-Pattern/src/main/java/com/MrLiu/fly/hungry/HungryBlockSingleton.java
+++ Singleton-Pattern/src/main/java/com/MrLiu/fly/hungry/HungryBlockSingleton.java
@@ -4,7 +4,7 @@ package com.MrLiu.fly.hungry;
  * @author liu_l
  * @email: liu_lei_programmer@163.com
  * @time 2019/4/16 11:32
- * @Description: 描述:单例—饿汉模式（在静态代码块中初始化）； 缺点：不管是否用到，都会在系统启动时实例化一个对象
+ * @Description: 描述:单例—饿汉模式（在静态代码块中初始化）； 缺点：不管是否用到，都会在系统启动时实例化一个对象。
  */
 public class HungryBlockSingleton {
 
