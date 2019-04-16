9bfc53abe6d3467b83a0f78870d018ff2658c5de
Mr.Liu <liu_lei_programmer@163.com>
2019-04-16 16:53:11 +0800

    设计模式—工厂模式

--- Factory-Pattern/src/main/java/com/MrLiu/fly/factoryMethod/CCourseFactory.java
+++ Factory-Pattern/src/main/java/com/MrLiu/fly/factoryMethod/CCourseFactory.java
@@ -7,7 +7,7 @@ import com.MrLiu.fly.model.ICourse;
  * @author liu_l
  * @email: liu_lei_programmer@163.com
  * @time 2019/4/4 10:47
- * @Description: 描述:
+ * @Description: 描述:。
  */
 public class CCourseFactory implements ICourseFactory {
     @Override
